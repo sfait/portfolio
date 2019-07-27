a78f935075be961b2de293cefbbd86b2050615dd
Sylwia Fait <sylwia.fait@gmail.com>
2019-07-27 16:10:45 +0200

    Update second header

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
+function getDate() {
+    const today = new Date();
+    let hours = today.getHours();
+
+    if (hours >= 6 && hours < 12) {
+        return "Miłego dnia!";
+    } else if (hours >= 12 && hours < 18) {
+        return "Miłego popołudnia!";
+    } else if (hours >= 18 && hours <= 21) {
+        return "Miłego wieczoru!";
+    } else {
+        return "Miłej nocy!";
+    }
+}
+
+function showHeader() {
+    const secondHeader = document.querySelector(".second-header");
+    secondHeader.textContent = getDate();
+}
+
 function getRandomColor() {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     return randomColor;
@@ -17,7 +37,14 @@ function changeColor() {
     }
 }
 
+// function checkSlide(e) {
+//     console.log(e);
+// }
+//
+// window.addEventListener("scroll", checkSlide);
+
 const init = () => {
+    showHeader();
     changeColor();
 };
 
